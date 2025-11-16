bb26f16ed7f49a61a02a5e8405b34dab69490f14
phoebebright <phoebebright310@gmail.com>
2025-11-16 17:39:43 +0000

    try to fix django.db.utils.ProgrammingError: relation "uniq_newsletter_email" already exists

--- skorie_news/migrations/0001_initial.py
+++ skorie_news/migrations/0001_initial.py
@@ -367,8 +367,8 @@ class Migration(migrations.Migration):
             unique_together={('slug', 'newsletter')},
          ),
         #django.db.utils.ProgrammingError: relation "uniq_newsletter_email" already exists
-        # migrations.AddConstraint(
-        #     model_name='subscription',
-        #     constraint=models.UniqueConstraint(condition=models.Q(('email__isnull', False)), fields=('newsletter', 'email'), name='uniq_newsletter_email'),
-        # ),
+        migrations.AddConstraint(
+            model_name='subscription',
+            constraint=models.UniqueConstraint(condition=models.Q(('email__isnull', False)), fields=('newsletter', 'email'), name='uniq_newsletter_email'),
+        ),
     ]
