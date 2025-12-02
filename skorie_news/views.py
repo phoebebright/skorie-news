66a8ede8a992eec117c83ede60eaf362afb3c7a2
phoebebright <phoebebright310@gmail.com>
2025-12-02 15:41:25 +0000

    removed rouge comma

--- skorie_news/views.py
+++ skorie_news/views.py
@@ -1022,7 +1022,7 @@ class IssueCreateView(UserCanAdministerMixin, FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["newsletter"] = self.newsletter,
+        context["newsletter"] = self.newsletter
         return context
 
     # ---- Handling the validated form ------------------------------------
