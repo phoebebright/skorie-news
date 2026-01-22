f1ff74b9650f4593ebdf3425709447c4f6cfec41
phoebebright <phoebebright310@gmail.com>
2026-01-22 20:39:46 +0000

    hack removed

--- skorie_news/models.py
+++ skorie_news/models.py
@@ -1702,8 +1702,7 @@ class Mailing(CreatedUpdatedMixin):
         batch_size = 400
         try:
             # 4) Chunk recipients to respect Mailgun limits
-            #for start in range(0, len(recipients), batch_size):
-            for start in range(401, len(recipients), batch_size):
+            for start in range(0, len(recipients), batch_size):
 
                 chunk = recipients[start:start + batch_size]
 
